e00937a42860caaeea49ecf67a654c4908923ace
rakaar <rka87338@gmail.com>
2023-06-24 01:10:51 +0530

    yeah drawer bug fixed

--- findSimilarMethods.js
+++ findSimilarMethods.js
@@ -1,46 +1,37 @@
-function calculateSimilarity(text1, text2) {
-    // Create a regular expression to match HTML tags
-    const regex = /(<([^>]+)>)/ig;
-
-    // Use the replace method to remove HTML tags from the content strings
-    text1 = text1.replace(regex, '');
-    text2 = text2.replace(regex, '');
-
+function calculateSimilarity(referenceText, largeText) {
     // Check if the inputs are strings, if not convert them to string
-    if (typeof text1 !== 'string') {
-        text1 = String(text1);
+    if (typeof referenceText !== 'string') {
+        referenceText = String(referenceText);
     }
-    if (typeof text2 !== 'string') {
-        text2 = String(text2);
+    if (typeof largeText !== 'string') {
+        largeText = String(largeText);
     }
 
-    // Define common English words to exclude
-    const commonWords = ["the", "a", "an", "of", "and", "is", "in", "it", "you", "that"];
+    // Regular expression to match numeric strings
+    const numericRegex = /^\d+$/;
 
-    // Convert both texts to lowercase, split into words and filter out common words
-    let words1 = text1.toLowerCase().split(/\W+/).filter(word => !commonWords.includes(word));
-    let words2 = text2.toLowerCase().split(/\W+/).filter(word => !commonWords.includes(word));
+    // Convert both texts to lowercase, split into words, and filter out numeric strings from the referenceText
+    let wordsReference = referenceText.toLowerCase().split(/\W+/).filter(word => !numericRegex.test(word));
+    let wordsLarge = largeText.toLowerCase().split(/\W+/);
 
-    // Count the number of occurrences of each word in both texts
-    let counts1 = words1.reduce(countOccurrences, {});
-    let counts2 = words2.reduce(countOccurrences, {});
+    let weight = 1; // Initial weight for the last word
+    let totalScore = 0; // Total score
 
-    // Sum the counts of the common words
-    let commonWordCount = Object.keys(counts1).reduce((sum, word) => {
-        if(word in counts2) {
-            sum += Math.min(counts1[word], counts2[word]);
-        }
-        return sum;
-    }, 0);
+    // Iterate over the words in the referenceText in reverse order
+    for (let i = wordsReference.length - 1; i >= 0; i--) {
+        // Calculate the probability of the word appearing in largeText
+        let wordOccurrences = wordsLarge.filter(word => word === wordsReference[i]).length;
+        let probability = wordOccurrences / wordsReference.length;
 
-    // Return the score
-    return commonWordCount;
+        totalScore += (weight * probability); // Add the weighted probability to the total score
+        weight /= 2; // Halve the weight for the next word
+    }
+
+    console.log("totalScrore ", totalScore)
+    return totalScore;
 }
 
-function countOccurrences(counts, word) {
-    counts[word] = (counts[word] || 0) + 1;
-    return counts;
-}
+
 
 function findSimilarMethods(text, objects) {
     // Calculate the similarity scores
--- handleMethods.js
+++ handleMethods.js
@@ -9,18 +9,25 @@ async function handleMethod(window_href, precedingText) {
         console.log("text is ", precedingText)
         console.log("most similar methods are ", mostSimilarMethods)
         // display in a drawer
+       // Remove any existing drawers
+        const existingDrawer = document.getElementById('sideDrawer');
+        if (existingDrawer) {
+        existingDrawer.remove();
+        }
+
         // Create a new div with a side drawer and cross button
         const drawer = document.createElement('div');
+        drawer.id = 'sideDrawer'; // Assign an id to the drawer
         drawer.style.position = 'fixed';
-        drawer.style.zIndex = 1000;  // make sure the drawer appears on top of all other elements
-        drawer.style.right = '0';  // align the drawer to the right side of the page
-        drawer.style.top = '0';    // align the drawer to the top of the page
-        drawer.style.height = '100vh';  // make the drawer full height
-        drawer.style.width = '40%';  // make the drawer 30% of the width of the page
-        drawer.style.backgroundColor = '#fff';  // adjust as needed
-        drawer.style.overflow = 'auto';  // add scrolling if the content is too long
-        drawer.style.padding = '20px';  // adjust as needed
-        drawer.style.boxShadow = '-2px 0 8px rgba(0, 0, 0, 0.2)';  // add a shadow effect
+        drawer.style.zIndex = 1000;
+        drawer.style.right = '0';
+        drawer.style.top = '0';
+        drawer.style.height = '100vh';
+        drawer.style.width = '40%';
+        drawer.style.backgroundColor = '#fff';
+        drawer.style.overflow = 'auto';
+        drawer.style.padding = '20px';
+        drawer.style.boxShadow = '-2px 0 8px rgba(0, 0, 0, 0.2)';
 
         // Begin constructing the HTML to be inserted into the drawer
         let drawerHTML = `
@@ -47,7 +54,7 @@ async function handleMethod(window_href, precedingText) {
             window.getSelection().removeAllRanges();
 
             // close the drawer
-            drawer.parentNode.removeChild(drawer);
+            drawer.remove();
         });
 
         
